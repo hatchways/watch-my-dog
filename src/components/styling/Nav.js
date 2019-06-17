32e8fa82043b25a95d536a189031f2aaec7c636c
Rhut Virani <rhut.virani@gmail.com>
2019-06-17 19:48:49 -0400

    Started from scratch

--- src/components/styling/Nav.js
+++ src/components/styling/Nav.js
@@ -1,49 +1,60 @@
-import React  from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, Toolbar} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import dogPaw from '../../static/images/dog-paw.png'
 
-// A theme based styling for Material UI
+
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    title: {
-      flexGrow: 1,
-    },
-    webpages:{
-        flexGrow: 1
-    },
-    link:{
-        marginLeft: theme.spacing(4),
-        color: "white",
-        textDecoration: "none",
+  root: {
+    flexGrow: 1,
+  },
+  navbar:{
+    background: 'transparent', 
+    boxShadow: 'none'
+  },
+  menuButton: {
+    marginLeft: theme.spacing(1),
+    backgroundImage: `url(${dogPaw})`,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+    color: "#1f1f1f",
+    fontWeight: "bold",
+  },
+  button:{
+    padding: "1% 3%",
+    margin: "1%",
+    color: "inherit",
+  },
+  sitterButton:{
+    textTransform: "smallcaps"
+  },
+  signup:{
+    backgroundColor: "#f04040",
+  }
+}));
 
-    }
-  }));
+export default function ButtonAppBar() {
+  const classes = useStyles();
 
-export default function Nav (props) {
-        const classes = useStyles();
-        return (
-            <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" className={classes.title}>
-                WatchMyDog
-              </Typography>
-              <Typography variant="h6" className={classes.webpages}>
-                  <Link to={"/"} className={classes.link}>
-                    Home
-                  </Link>
-                  <Link to={"/landingpage"} className={classes.link}>
-                    Profile
-                  </Link>
-              </Typography>
-              {/* <Button color="inherit">Login</Button>
-              <Button color="inherit">Signup</Button> */}
-            </Toolbar>
-          </AppBar>      
-        )
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.navbar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Lovingsitter
+          </Typography>
+          <Button size="medium" disableRipple="true" disableFocusRipple="true"  className={`${classes.button} + ${classes.sitterButton}`}><u>Become a Sitter</u></Button>
+          <Button size="medium" variant="outlined" className={`${classes.button} + ${classes.login}`}>Login</Button>
+          <Button size="medium" className={`${classes.button} + ${classes.signup}`}>Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
