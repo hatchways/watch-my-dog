3688f0808178748efa831a05360d45fd124e39e8
Rhut Virani <rhut.virani@gmail.com>
2019-08-01 18:06:19 -0400

    corrected gitignore

---
+++ client/build/precache-manifest.92f1ba808b5e9197d8700b8de85ef53f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e880db3028bd429f1b6097689c2a747b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85d654532d60307437a",
+    "url": "/static/css/2.6d2c8d95.chunk.css"
+  },
+  {
+    "revision": "735e3661cf9ebdb49ccc",
+    "url": "/static/css/main.00d88364.chunk.css"
+  },
+  {
+    "revision": "f85d654532d60307437a",
+    "url": "/static/js/2.e2b77ab2.chunk.js"
+  },
+  {
+    "revision": "735e3661cf9ebdb49ccc",
+    "url": "/static/js/main.b6db4060.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "889a4b5ce3a780faedb74390fc49a07a",
+    "url": "/static/media/Gilroy-Bold.889a4b5c.ttf"
+  },
+  {
+    "revision": "25e5ce9784cf3324af89cfd13fa59517",
+    "url": "/static/media/Gilroy-Light.25e5ce97.ttf"
+  },
+  {
+    "revision": "d8ee4539e9ba9211a8b532e325075577",
+    "url": "/static/media/Gilroy-Medium.d8ee4539.ttf"
+  },
+  {
+    "revision": "22d25e11ebe6b43e5a9bc87d0a0ccb61",
+    "url": "/static/media/Gilroy-Regular.22d25e11.ttf"
+  },
+  {
+    "revision": "66fa8a3d21e3bb3750feea2743c5cfe2",
+    "url": "/static/media/dog-mainpage.66fa8a3d.png"
+  },
+  {
+    "revision": "3a0182e3440d4c67d90e038e2c4f9136",
+    "url": "/static/media/home.3a0182e3.jpg"
+  }
+]);
